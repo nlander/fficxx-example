ba0f0da7448775cd93f0f0ed29c314b01dde6388
Nathan Lander <lander89@gmail.com>
2018-10-21 03:28:28 -0500

    Make example compile

--- mysample-generator/example/src/Main.hs
+++ mysample-generator/example/src/Main.hs
@@ -1,30 +1,32 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
-import           FFICXX.Generate.Builder
-import           FFICXX.Generate.Type.Class
-import           FFICXX.Generate.Type.Module
-import           FFICXX.Generate.Type.PackageInterface
+import           Data.HashMap.Strict (singleton)
+
+import           FFICXX.Generate.Builder (simpleBuilder)
+import           FFICXX.Generate.Code.Primitive (void_, cppclass_, cppclass)
+import           FFICXX.Generate.Type.Cabal (Cabal(..), CabalName(..), AddCInc(..), AddCSrc(..))
+import           FFICXX.Generate.Type.Class (Class(..), Function(Virtual, Constructor))
+import           FFICXX.Generate.Type.Config (ModuleUnit(MU_TopLevel), emptyModuleUnitImports, ModuleUnitMap(..))
 
 incs = [ AddCInc "test.h" "//test ok?" ]
 
 srcs = [ AddCSrc "test.cpp" "//test ok??" ]
 
 
-mycabal = Cabal { cabal_pkgname = "MySample"
+mycabal = Cabal { cabal_pkgname = CabalName "MySample"
+                , cabal_version = "2.4.0.0"
                 , cabal_cheaderprefix = "MySample"
                 , cabal_moduleprefix = "MySample"
                 , cabal_additional_c_incs = incs
                 , cabal_additional_c_srcs = srcs
+                , cabal_additional_pkgdeps = []
+                , cabal_license = Just "BSD3"
+                , cabal_licensefile = Just "LICENSE"
+                , cabal_extraincludedirs = []
+                , cabal_extralibdirs = []
+                , cabal_extrafiles = []
+                , cabal_pkg_config_depends = []
                 }
-
-mycabalattr = 
-    CabalAttr 
-    { cabalattr_license = Just "BSD3"
-    , cabalattr_licensefile = Just "LICENSE"
-    , cabalattr_extraincludedirs = []
-    , cabalattr_extralibdirs = []
-    , cabalattr_extrafiles = []
-    }
 
 
 myclass = Class mycabal
@@ -33,13 +35,13 @@ a :: Class
 a = myclass "A" [] mempty Nothing
     [ Constructor [] Nothing
     , Virtual void_ "method1" [ ] Nothing
-    ]
+    ] [] []
 
 b :: Class
 b = myclass "B" [a] mempty Nothing
     [ Constructor [] Nothing
     , Virtual (cppclass_ a) "method2" [ cppclass a "x" ] Nothing
-    ]
+    ] [] []
 
 myclasses = [ a, b]
 
@@ -48,5 +50,5 @@ toplevelfunctions = [ ]
 
 main :: IO ()
 main = do 
-  simpleBuilder "MySample" [] (mycabal,mycabalattr,myclasses,toplevelfunctions,[]) [ ] []
+  simpleBuilder "MySample" (ModuleUnitMap $ singleton MU_TopLevel emptyModuleUnitImports) (mycabal,myclasses,toplevelfunctions,[]) [ ] []
 
